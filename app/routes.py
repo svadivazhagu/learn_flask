6bc6d6312b5ec32abb59026eb67c6227b56e1929
Surya Vadivazhagu <svadivazhagu@wpi.edu>
2019-06-05 14:39:54 -0400

    finished login. added support for preventing login w/o credentials, and UI notification of no information. Added flash for user trying to login with information. need to add actual users.

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import LoginForm
 
 #decorator where user indicates what routes will lead to function
 @app.route('/')
@@ -19,4 +20,18 @@ def index():
       }
    ]
    return render_template('index.html', title = 'Hello', user=user, posts = posts)
+
+#decorator for login form
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    #creating a new login form to be passed into the render_template()
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    #form=form meaning the LoginForm that was instantiated as form is being passed into the template's form parameter
+    return render_template('login.html', title= 'Sign In', form = form)
+
+
 
